7f4b77d1d647bf99bb33a6d582fb5cfd1281d72b
Yurui-Feng <yurifeng@bu.edu>
2023-07-20 13:25:56 -0700

    implment mongoose encrypt

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-//jshint esversion:6
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -12,6 +13,10 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 });
+
+const secret = process.env.SECRET;
+
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -34,7 +39,6 @@ app
       const password = req.body.password;
       const foundUser = await User.findOne({
         email: username,
-        password: password,
       });
       if (foundUser) {
         if (foundUser.password === password) {
